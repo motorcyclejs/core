dd137288b360372d6825acaef00efdd6fe686649
TylorS <tlsteinberger167@gmail.com>
2015-11-13 17:15:48 -0500

    const invasion

--- src/index.js
+++ src/index.js
@@ -1,65 +1,72 @@
 import Subject from './subject'
 
-function makeSinkProxies(drivers) {
-  let requestProxies = {}
-  for (let name in drivers) {
-    if (drivers.hasOwnProperty(name)) {
-      requestProxies[name] = Subject()
-    }
-  }
-  return requestProxies
-}
-
-function callDrivers(drivers, requestProxies) {
-  let responses = {}
-  for (let name in drivers) {
-    if (drivers.hasOwnProperty(name)) {
-      responses[name] = drivers[name](requestProxies[name], name)
-    }
-  }
-  return responses
-}
-
-function replicateMany(observables, subjects) {
-  setTimeout(() => {
-    for (let name in observables) {
-      if (observables.hasOwnProperty(name) &&
-        subjects.hasOwnProperty(name))
-      {
-        observables[name].forEach(subjects[name].push)
+const makeSinkProxies =
+  drivers => {
+    let sinkProxies = {}
+    for (let name in drivers) {
+      if (drivers.hasOwnProperty(name)) {
+        sinkProxies[name] = Subject()
       }
     }
-  }, 1)
-}
+    return sinkProxies
+  }
 
-function isObjectEmpty(obj) {
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      return false
+const callDrivers =
+  (drivers, sinkProxies) => {
+    let sources = {}
+    for (let name in drivers) {
+      if (drivers.hasOwnProperty(name)) {
+        sources[name] = drivers[name](sinkProxies[name], name)
+      }
     }
-  }
-  return true
-}
-
-function run(main, drivers) {
-  if (typeof main !== `function`) {
-    throw new Error(`First argument given to Cycle.run() must be the 'main' ` +
-      `function.`)
-  }
-  if (typeof drivers !== `object` || drivers === null) {
-    throw new Error(`Second argument given to Cycle.run() must be an object ` +
-      `with driver functions as properties.`)
-  }
-  if (isObjectEmpty(drivers)) {
-    throw new Error(`Second argument given to Cycle.run() must be an object ` +
-      `with at least one driver function declared as a property.`)
+    return sources
   }
 
-  let sinkProxies = makeSinkProxies(drivers)
-  let sources = callDrivers(drivers, sinkProxies)
-  let sinks = main(sources)
-  replicateMany(sinks, sinkProxies)
-  return {sinks, sources}
-}
+const replicateMany =
+  (sinks, sinkProxies) =>
+    setTimeout(
+      () => {
+        for (let name in sinks) {
+          if (sinks.hasOwnProperty(name) &&
+          sinkProxies.hasOwnProperty(name))
+          {
+            sinks[name].forEach(sinkProxies[name].push)
+          }
+        }
+      }
+      , 1
+    )
+
+const isObjectEmpty =
+  obj => {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        return false
+      }
+    }
+    return true
+  }
+
+const run =
+  (main, drivers) => {
+    if (typeof main !== `function`) {
+      throw new Error(`First argument given to Cycle.run() must be the ` +
+        `'main' function.`)
+    }
+    if (typeof drivers !== `object` || drivers === null) {
+      throw new Error(`Second argument given to Cycle.run() must be an  ` +
+        `object with driver functions as properties.`)
+    }
+    if (isObjectEmpty(drivers)) {
+      throw new Error(`Second argument given to Cycle.run() must be an ` +
+        `object with at least one driver function declared as a property.`)
+    }
+
+    let sinkProxies = makeSinkProxies(drivers)
+    let sources = callDrivers(drivers, sinkProxies)
+    let sinks = main(sources)
+    replicateMany(sinks, sinkProxies)
+    return {sinks, sources}
+  }
 
 export {run}
